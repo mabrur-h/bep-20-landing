9fec37fc95db736effb6630a6cfb0bce1557c4cb
mabrur-h <mabrur.h11@gmail.com>
2022-02-05 17:02:23 +0500

    setup main components

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import {Component} from "react";
+
+class App extends Component {
+    // constructor(props) {
+    //   super(props);
+    // }
+
+    render() {
+        return (
+            <div className="App">
+                <h1 className="bg-success">Hello</h1>
+
+            </div>
+        );
+    }
+
+}
+
+export default App;
